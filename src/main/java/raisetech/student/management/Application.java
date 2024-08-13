9581ae80aa746b4e437983e4dd7e73ec8e434ba3
Kotaro Ito <nyarukotaro_65005@icloud.com>
2024-08-14 03:40:35 +0900

    アパッチ追加

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,5 +1,6 @@
 package raisetech.student.management;
 
+import org.apache.catalina.util.StringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
